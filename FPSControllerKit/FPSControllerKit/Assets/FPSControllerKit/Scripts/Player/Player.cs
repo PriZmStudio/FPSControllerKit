944f34482dec0a00aee299064ce353e0b418f1a6
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2015-11-03 00:14:00 +0530

    Player Sprinting and Crouching

--- FPSControllerKit/FPSControllerKit/Assets/FPSControllerKit/Scripts/Player/Player.cs
+++ FPSControllerKit/FPSControllerKit/Assets/FPSControllerKit/Scripts/Player/Player.cs
@@ -2,12 +2,109 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	
+	//other objects accessed by this script
+	[HideInInspector]
+	public GameObject weaponCameraObj;
+	[HideInInspector]
+	public GameObject weaponObj;
+	[HideInInspector]
+	public GameObject painFadeObj;
+	[HideInInspector]
+	public GameObject levelLoadFadeObj;
+	[HideInInspector]
+	public GameObject healthGuiObj;//this object is instantiated for heath display on hud
+	[HideInInspector]
+	public GameObject healthGuiObjInstance;
+	[HideInInspector]
+	public GameObject helpGuiObj;//this object is instantiated for help text display
+	[HideInInspector]
+	public GameObject helpGuiObjInstance;
+	[HideInInspector]
+	public GameObject PickUpGuiObj;//this object is instantiated for hand pick up crosshair on hud
+	[HideInInspector]
+	public GameObject PickUpGuiObjGuiObjInstance;
+	[HideInInspector]
+	public GameObject CrosshairGuiObj;//this object is instantiated for aiming reticle on hud
+	[HideInInspector]
+	public GameObject CrosshairGuiObjInstance;
+	[HideInInspector]
+	public Projector shadow;//to access the player shadow projector 
+	private AudioSource[]aSources;//access the audio sources attatched to this object as an array for playing player sound effects
+	
+	//player hit points
+	public float hitPoints = 100.0f;
+	public float maximumHitPoints = 200.0f;
+	
+	//Damage feedback
+	private float gotHitTimer = -1.0f;
+	public Color PainColor = new Color(0.75f, 0f, 0f, 0.5f);//color of pain screen flash can be selected in editor
+	
+	//crosshair 
+	public bool crosshairEnabled = true;//enable or disable the aiming reticle
+	private bool crosshairVisibleState = true;
+	private bool crosshairTextureState = false;
+	public Texture2D Reticle;//the texture used for the aiming crosshair
+	public Texture2D Hand;//the texture used for the pick up crosshair
+	private Color handColor = Color.white; 
+	private Color reticleColor = Color.white; 
+	[HideInInspector]
+	public LayerMask rayMask = 0;//only layers to include for crosshair raycast in hit detection (for efficiency)
+	
+	//button and behavior states
+	private bool pickUpBtnState = true;
+	[HideInInspector]
+	public bool restarting = false;//to notify other scripts that level is restarting
+	
+	//zooming
+	private bool zoomBtnState = true;
+	private float zoomStopTime = 0.0f;//track time that zoom stopped to delay making aim reticle visible again
+	[HideInInspector]
+	public bool zoomed = false;
+	private float zoomStart = -2.0f;
+	private bool zoomStartState = false;
+	private float zoomEnd = 0.0f;
+	private bool zoomEndState = false;
+	
+	//sound effects
+	public AudioClip painLittle;
+	public AudioClip painBig;
+	public AudioClip die;
 
 	[Header("Movement Settings:")]
+	//player controls set in the inspector
 	public KeyCode moveForward;
 	public KeyCode moveBack;
 	public KeyCode strafeLeft;
 	public KeyCode strafeRight;
+	public KeyCode jump;
+	public KeyCode crouch;
+	public KeyCode sprint;
+	[Header("Weapon Settings:")]
+	public KeyCode fire;
+	public KeyCode zoom;
+	public KeyCode reload;
+	public KeyCode fireMode;
+	public KeyCode holsterWeapon;
+	public KeyCode selectNextWeapon;
+	public KeyCode selectPreviousWeapon;
+	public KeyCode selectWeapon1;
+	public KeyCode selectWeapon2;
+	public KeyCode selectWeapon3;
+	public KeyCode selectWeapon4;
+	public KeyCode selectWeapon5;
+	public KeyCode selectWeapon6;
+	public KeyCode selectWeapon7;
+	public KeyCode selectWeapon8;
+	public KeyCode selectWeapon9;
+	public KeyCode selectWeapon10;
+	[Header("Misc Settings:")]
+	public KeyCode use;
+	public KeyCode moveObject;
+	public KeyCode throwObject;
+	public KeyCode showHelp;
+	public KeyCode restartScene;
+	public KeyCode exitGame;
 	void Start () {
 	
 	}
--- FPSControllerKit/FPSControllerKit/Assets/FPSControllerKit/Scripts/Player/PlayerMovement.cs
+++ FPSControllerKit/FPSControllerKit/Assets/FPSControllerKit/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	
+	public AudioClip landfx;//audiosource attatched to this game object with landing sound effect
 	//falling
 	[HideInInspector]
 	public float airTime = 0.0f;//total time that player is airborn
@@ -18,8 +19,7 @@ public class PlayerMovement : MonoBehaviour {
 	private bool falling = false;//true when player is losing altitude
 	[HideInInspector]
 	public bool grounded = false;// true when the capsule hits the ground
-	private bool airTimeState = false;
-	//jumping
+
 	//jumping
 	public float antiBunnyHopFactor = 0.35f;//to limit the time between player jumps
 	[HideInInspector]
@@ -29,6 +29,37 @@ public class PlayerMovement : MonoBehaviour {
 	private bool jumpBtn = true;//to control jump button behavior
 	[HideInInspector]
 	public float landStartTime = 0.0f;//time that player landed from jump
+
+	//crouching	
+	[HideInInspector]
+	public float midPos = 0.9f;//camera vertical position
+	[HideInInspector]
+	public bool crouched = false;//true when player is crouching
+	private bool crouchState = false;
+	private bool crouchHit = false;//true when object above player prevents standing up from crouch
+
+	
+	//sprinting
+	[HideInInspector]
+	public bool canRun = true;//true when player is allowed to sprint
+	[HideInInspector]
+	public bool sprintActive = false;//true when sprint button is ready
+	private bool sprintBtnState = false;
+	[HideInInspector]
+	public bool cancelSprint = false;//true when sprint is canceled by other player input
+	[HideInInspector]
+	public float sprintStopTime = 0.0f;//track when sprinting stopped for control of item pickup time in FPSPlayer script 
+	private bool sprintStopState = true;
+
+	
+	//climbing (ladders or other climbable surfaces)
+	[HideInInspector]
+	public bool climbing = false;//true when playing is in contact with ladder trigger
+	public float climbSpeed = 4.0f;//speed that player moves upward when climbing
+	[HideInInspector]
+	public float climbSpeedAmt = 4.0f;//actual rate that player is climbing
+
+
 	//track player input
 	[HideInInspector]
 	public float inputXSmoothed = 0.0f;//binary inputs smoothed using lerps
@@ -53,6 +84,13 @@ public class PlayerMovement : MonoBehaviour {
 	private float crouchSpeedAmt = 1.0f;
 	public float strafeSpeedPercentage = 0.8f;//percentage to decrease movement speed while strafing directly left or right
 	public float zoomSpeedPercentage = 0.6f;//percentage to decrease movement speed while zooming
+
+	
+	//sound effeects
+	public AudioClip landfx;//audiosource attatched to this game object with landing sound effect
+	public AudioClip jumpfx;//audiosource attatched to this game object with jumping sound effect
+	public LayerMask clipMask;//mask for reducing the amount of objects that ray and capsule casts have to check
+
 
 	void Awake()
 	{
@@ -117,7 +155,100 @@ public class PlayerMovement : MonoBehaviour {
 				fallingDistance=0;
 				landStartTime=Time.time;
 				falling=false;
+
+				if((fallStartLevel - playerTransform.position.y)>2.0f)
+				{
+					//play landing sound effect when falling and not landing from jump
+					if(!jumping)
+					{
+						//Play Landing sound
+						AudioSource.PlayClipAtPoint(landfx, Camera.main.transform.position);
+						//make camera jump when landing for better feeling of player weight	
+						if (Camera.main.GetComponent<Animation>().IsPlaying("CameraLand"))
+						{
+							//rewind animation if already playing to allow overlapping playback
+							Camera.main.GetComponent<Animation>().Rewind("CameraLand");
+						}
+						Camera.main.GetComponent<Animation>().CrossFade("CameraLand", 0.35f,PlayMode.StopAll);
+					}
+				}
+				// Calculate the distance of the fall and apply damage likewise
+				if(playerTransform.position.y < fallStartLevel - fallingDamageThreshold)
+				{
+					CalculateDamage(fallStartLevel - playerTransform.position.y);
+				}
 			}
+
+			//-----------------------------------------
+			// Crouch
+			//-----------------------------------------
+
+			if(Input.GetKey(PlayerComponent.crouch))
+			{
+				if(!crouchState)
+				{
+					if(!crouched)
+					{
+						crouched=true;
+						sprintActive=false; // cancel sprinting while crouching
+					}
+					else
+					{
+						if(!Physics.CapsuleCast (p1, p2, capsule.radius * 0.9f, transform.up, out hit, 0.4f, clipMask.value))
+						{
+							// ????
+							crouched = false;
+						}
+					}
+					crouchState=true;
+				}
+			}
+			else
+			{
+				crouchState=false;
+				if(sprintActive || climbing)
+				{
+					crouched=false;
+				}
+			}
+			//cancel crouch if jump is pressed
+			if(Input.GetKey(PlayerComponent.jump) && crouched && !Physics.CapsuleCast (p1, p2, capsule.radius * 0.9f, transform.up, out hit, 0.4f, clipMask.value))
+			{
+				crouched = false;
+				landStartTime = Time.time;//set land time to time jump is pressed to prevent uncrouching and then also jumping
+			}
+			
+			//-----------------------------------------
+			// Sprinting
+			//-----------------------------------------
+
+
+			// setting up of sprint on different input conditions
+			if(Input.GetKey(PlayerComponent.sprint))
+			{
+				if(sprintBtnState)
+				{
+					if(!sprintActive && !crouchHit) // if we are not sprinting and we are not under any collider
+					{
+						sprintActive=true;
+					}
+					else
+					{
+						sprintActive=false; // pressing sprint button while sprinting will stop the sprint operation
+					}
+					sprintBtnState=false;
+				}
+			}
+			else
+			{
+				sprintBtnState=true;
+				if(!canRun)
+				{
+					sprintActive=false;
+				}
+			}
+
+			//cancel sprint operation in certain cases
 		}
 	}
 }
